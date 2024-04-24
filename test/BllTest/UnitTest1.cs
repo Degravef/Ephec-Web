7f9fdead2eb18b24746fefa7d6c819662f4c8793
Degravef <00_dot.mahjong@icloud.com>
2024-04-24 20:15:00 +0200

    Init Backend

---
+++ test/BllTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BllTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ test/DalTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DalTest_;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
