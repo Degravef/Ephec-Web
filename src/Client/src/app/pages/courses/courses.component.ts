8e95e84e68c25fea5886791d336ec210dea0a560
Degravef <00_dot.mahjong@icloud.com>
2024-05-17 07:53:34 +0200

    Add table view and data retrieval for courses
    Updated the courses component to include a table view for displaying course data. Enhanced data handling by implementing an HTTP GET request to fetch course details from the backend, and updated package dependencies as necessary.

--- src/Client/src/app/pages/courses/courses.component.ts
+++ src/Client/src/app/pages/courses/courses.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from "@angular/core";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  imports: [
+    NgForOf
+  ],
+  styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  private http: HttpClient;
+  protected courses: any;
+
+  constructor(_http: HttpClient) {
+    this.http = _http;
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>("https://localhost:7167/api/Admin/users")
+      .subscribe(data => this.courses = data);
+  }
 }
